5643ce1c735e5505b3c76fd9570b130b07993d96
Jakib9999 <jakib9999@gmail.com>
2023-01-09 10:29:01 +0600

    Update settings.py

--- test1/settings.py
+++ test1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e&p+y(*+mwh6372^90*8&4bs1y1dj*dik#o*l2x@x#s#_jsp%a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
